2a3fca152f1a27fe99b0ab24a0ae75236916907c
Andrei Suciu <andreisuciu2005@yahoo.com>
2023-03-27 18:12:11 +0300

    Fix

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const config = require('./config/config');
@@ -17,11 +18,11 @@ app.use('/users', require('./routes/users'));
 
 // Serve static assets if in production
 if (config.ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 // Start the server
